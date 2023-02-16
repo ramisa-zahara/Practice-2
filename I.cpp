6185ea34f7ec31549d0a59fb3972714289262151
ramisa-zahara <ramisazahara@gmail.com>
2023-02-16 22:38:40 +0600

    problem K

--- I.cpp
+++ I.cpp
@@ -16,4 +16,11 @@ int main()
     cout<<"YES\n";
     else
     cout<<"NO\n";
+    /* For example take 121
+    n=121; ans=121 will be stored
+    digit = (121%10)=1
+    reverse=(0*10)+1
+    n=121/10=12 ( this loop will execute untill n is not equal to zero.)
+    if reverse is equal to n (which is stored in ans) then the number is a palindrome.
+    */
 }
