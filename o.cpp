27ef1f4dccc39c2be25add4a8c8d7c205dca8cca
ramisa-zahara <ramisazahara@gmail.com>
2023-06-10 02:40:59 +0600

    problem O

---
+++ o.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
