de3d6ab2fb1cd6e78d4f7306991288dab9a74d41
ramisa-zahara <ramisazahara@gmail.com>
2023-06-10 02:27:06 +0600

    problem L using gcd builtin function

--- A.cpp
+++ A.cpp
@@ -1,11 +1,60 @@
 #include<bits/stdc++.h>
 #include<iomanip>
 using namespace std;
+#define ll long long
+#define infLL 1e18
+const int mx=512;
+ll dist[mx][mx];
+
+
+void FLOYD_WARSHALL(int n)
+{
+   for(int k=1;k<=n;++k)
+   {
+      for(int i=1;i<=n;++i)
+      {
+         for(int j=1;j<=n;j++)
+         {
+            if(dist[i][k]+dist[k][j]<dist[i][j])
+            {
+               dist[i][j]=dist[i][k]+dist[k][j];
+            }
+         }
+      }
+
+   }
+
+   for(int i=1;i<=n;++i)
+   {
+      cout<<"Min cost "<<i<<endl;
+      for(int j=1;j<=n;++j){
+         cout<<dist[i][j]<<"\t";
+      }
+   }
+}
+
+
 int main()
 {
-   int n;
-   cin>>n;
-   for(int i=1;i<=n;i++)
-    cout<<i<< "\n";
-    return 0;
+   int n,m;
+   cin>>n>>m;
+   for(int i=1;i<=n;++i){
+      for(int j=1;j<=n;++j)
+      {
+         if(i!=j)
+            dist[i][j]=infLL;
+      }
+   }
+
+   for(int i=1;i<=m;++i)
+   {
+      int u,v,w;
+      cin>>u>>v>>w;
+      dist[u][v]=min(dist[u][v], (ll)w);
+      dist[v][u]=min(dist[v][u],(ll)w);
+   }
+
+   FLOYD_WARSHALL(n);
+
+   return 0;
 }
---
+++ l.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin>> A>> B;
+    int gcd = __gcd(A,B);
+    cout<<gcd<<"\n";
+}
